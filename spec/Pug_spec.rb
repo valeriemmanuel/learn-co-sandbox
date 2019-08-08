a4e367b9d35ee07300f633e72d80beddb68d9fc5
Valerie Manuel <valerie.manuel11@gmail.com>
2019-08-08 23:37:53 +0000

    added test to the spec

---
+++ spec/Pug_spec.rb
@@ -0,0 +1,20 @@
+describe Pug do
+  context 'properties' do
+    it 'has a breed' do
+      pug = Pug.new
+      pug. =
+    end
+    it 'has a name' do
+      pug = Pug.new
+      pug. =
+    end
+    it 'has an age' do
+      pug = Pug.new
+      pug. =
+    end
+    it 'has a gender' do
+      pug = Pug.new
+      pug. =
+    end
+  end
+end
