91f6cb29af02d9e698606491a0e708cad29f2df4
Valerie Manuel <valerie.manuel11@gmail.com>
2019-08-08 23:47:31 +0000

    fleshed out spec test

--- spec/Pug_spec.rb
+++ spec/Pug_spec.rb
@@ -2,19 +2,23 @@ describe Pug do
   context 'properties' do
     it 'has a breed' do
       pug = Pug.new
-      pug. =
+      pug.breed = "Breed"
+      expect (pug.breed). to eq("Breed")
     end
-    it 'has a name' do
+    it 'has a Name' do
       pug = Pug.new
-      pug. =
+      pug.name = "Name"
+      expect (pug.name).to eq("Name")
     end
     it 'has an age' do
       pug = Pug.new
-      pug. =
+      pug.age = "Age"
+      expect  (pug.age).to eq("Age")
     end
     it 'has a gender' do
       pug = Pug.new
-      pug. =
+      pug.gender = "Gender"
+      expect (pug.gender).to eq ("Gender")
     end
   end
 end
