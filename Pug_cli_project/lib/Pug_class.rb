b30ec0c1afa606eeec170e3097787b180e028382
Valerie Manuel <valerie.manuel11@gmail.com>
2019-08-09 00:28:13 +0000

    complete spec but with and error "unititialized constant Pug"

--- Pug_cli_project/lib/Pug_class.rb
+++ Pug_cli_project/lib/Pug_class.rb
@@ -1,10 +1,3 @@
 class Pug
-  
-  def initialize (name)
-    @name = name
-  end
-    
-    def name
-      @name
-    end
+ attr_accessor :name, :breed, :age, :gender
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require_relative'../lib/Pug_class
